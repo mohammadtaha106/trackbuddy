1f3f09e21490d1be2dadcae8d0ef811af98c8996
mohammadtaha106 <mohammadtaha19888@gmail.com>
2024-10-24 09:03:58 -0700

    DONE

--- src/app/(admin)/admin/dashboard/page.js
+++ src/app/(admin)/admin/dashboard/page.js
@@ -1,7 +1,9 @@
+import AnalyticsChart from "@/components/BarChart/BarChart";
+
 export default function Dashboard() {
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-4xl font-bold">Dashboard</h1>
+      <div className=" min-h-screen ">
+     <AnalyticsChart/>
       </div>
     );
   }
--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">Welcome to TrackMate!</h1>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
+      <h1 className="text-4xl font-bold">Welcome to TrackBuddy!</h1>
       <Button>Click Me</Button>
     </div>
   );
