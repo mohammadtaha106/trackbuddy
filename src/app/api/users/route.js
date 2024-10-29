ad07da74cff734f0b4f357b28c7d6134ceda0e61
mohammadtaha106 <mohammadtaha19888@gmail.com>
2024-10-29 13:26:27 -0700

    get post api work done

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,30 +1,35 @@
-import { connectDB } from "@/app/db/connectDB";
-import { UserModal } from "@/app/db/models/User";
+import { connectDB } from "@/lib/db/connectDB";
+import { UserModal } from "@/lib/models/User";
+
+
 
 export async function GET(request) {
-    await connectDB()
-    const users = await UserModal.find()
-    return Response.json(
-        {
-            msg:'Users Fetched Successfully',
-            users
-        },
-        { status: 200 })
+  await connectDB();
+  const users = await UserModal.find();
+  return Response.json(
+    {
+      msg: "Users Fetched Successfully",
+      users,
+    },
+    { status: 200 }
+  );
 }
 
- 
 export async function POST(request) {
-    await connectDB()
-    let obj = await request.json()
-    let newUser = await UserModal.find(obj)
-    await newUser.save();
-    return Response.json({
-        msg:'Users Added Successfully',
-        user: newUser
-    },{ status: 201 }
-)
+  await connectDB();
+  const obj = await request.json();
+  let newUser = new UserModal(obj);
+  await newUser.save();
+
+  return Response.json(
+    {
+      msg: "Users Added Successfully ",
+      user: newUser,
+    },
+    { status: 201 }
+  );
 }
- 
+
 export async function PUT(request) {}
- 
+
 export async function DELETE(request) {}
